13c6cd08bbb3cec13ac865d19e85515ed358a84d
toine <anthony.vandenbroele@gmail.com>
2023-12-07 20:02:40 +0100

    .\oefening2.py

---
+++ oefening2.py
@@ -0,0 +1,11 @@
+#Gebruiker vragen om twee getallen in te voeren
+#vervolgens de som en het product van die getallen berekenen en weergeven. 
+
+getal1 = int(input("Geef een getal in: "))
+getal2 = int(input("Geef nog een getal in: "))
+
+som = getal1 + getal2
+product = getal1 * getal2
+
+print(f"De som van", getal1, "&", getal2 ,"is", som, "!")
+print(f"Het product van", getal1, "&", getal2 ,"is", product, "!")
